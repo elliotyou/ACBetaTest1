2ed77d4b0bda0280d135e9ea4a38c6486672c118
elliotyou <m9402311@mail.ntust.edu.tw>
2021-10-26 20:42:10 +0800

    U9 feat: flash msg & singup verification

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/restaurants', restController.getRestaurant)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-router.get('/restaurants', restController.getRestaurant)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
