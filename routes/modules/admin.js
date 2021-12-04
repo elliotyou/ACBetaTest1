2b2d564fe21e8942897dd51f24a8a76bd0108856
elliotyou <m9402311@mail.ntust.edu.tw>
2021-12-04 20:36:54 +0800

    U36, feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -13,6 +13,9 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant) // 修改後台編輯餐廳的路由
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
