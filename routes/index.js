e0794ee91f72bea4c97680363d907beeee330e82
elliotyou <m9402311@mail.ntust.edu.tw>
2021-12-18 16:21:03 +0800

    U45, feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/​​comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
@@ -18,6 +19,7 @@ router.get('/restaurants/:id/dashboard', authenticated, restController.getDashBo
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.post('/comments', authenticated, commentController.postComment)
 router.use('/', generalErrorHandler)
 
 module.exports = router
