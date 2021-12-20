1567ebcea6f60b170d649371559fb79e7cf2970d
elliotyou <m9402311@mail.ntust.edu.tw>
2021-12-20 22:14:48 +0800

    U47.5 A3 User Profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,61 @@
 const bcrypt = require('bcryptjs')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 // const { noExtendLeft } = require('sequelize/types/lib/operators')
 const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = db
 const userController = {
+  getUser: (req, res, next) => {
+    Promise.all([
+      User.findByPk(req.params.id, { raw: true }),
+      Comment.findAndCountAll({
+        include: [User, Restaurant],
+        where: { userId: req.params.id },
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([user, comments]) => {
+        if (!user) throw new Error("User doesn't exsist")
+        return res.render('profile', {
+          user,
+          comments: comments.rows,
+          commentCount: comments.count
+        })
+      })
+    // User.findByPk(req.params.id, { raw: true })
+    //   .then(user => {
+    //     if (!user) throw new Error("User doesn't exsist")
+    //     return res.render('profile', { user })
+    //   })
+  },
+  editUser: (req, res, next) => {
+    User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist")
+        return res.render('profile-form', { user })
+      })
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('User name is required!')
+    const { file } = req
+    Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'user profile was successfully updated')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
+  },
   signUpPage: (req, res) => {
     res.render('signup')
   },
